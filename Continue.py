b4b5ee272028d296c8e5d26aa4d8b4f768932f7e
Lili-wang-del <100299wll@gmail.com>
2025-03-22 13:07:34 +0800

    Delete Continue.py

--- Continue.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# 断点续训
-# Load a model
-model = YOLO('D:/下载/ultralytics-20240303/ultralytics-main/runs/detect/train5/weights/last.pt')  # load a partially trained model
-
-# Resume training
-results = model.train(resume=True)
